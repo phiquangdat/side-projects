6c6da1b33545ed159a1cede7cdfe53a075b685c2
phiquangdat <datphi020603@gmail.com>
2025-02-21 00:14:05 +0200

    finish the delete button

--- budget-tracker-app/script.js
+++ budget-tracker-app/script.js
@@ -3,23 +3,28 @@ $(document).ready(function () {
   let income = 0;
   let expense = 0;
   let type;
+
   $("#addTransaction").click(function () {
     let description = $("#description").val();
     let sum = Number($("#sum").val());
+    type = sum < 0 ? "expense" : "income";
+
     balance += sum;
     if (sum < 0) {
-      expense += sum * -1;
+      expense += Math.abs(sum);
     } else {
       income += sum;
     }
+
     $("#balance").html(balance);
+
     $("#transaction-list").append(`
             <li class="${type}">
                 <p class="description">${description}</p>
                 <p class="sum">${sum}</p>
                 <button class="delete">Poista</button>
             </li>
-        `);
+    `);
 
     //Add to the chart the data of income and expense columns
     myChart.data.datasets[0].data[0] = income;
@@ -28,20 +33,32 @@ $(document).ready(function () {
 
     $("#description").val("");
     $("#sum").val("");
-    window.location.reload();
   });
 
-  $(".delete").click(function () {
-    let amount = Number($(this).closest(".sum").val());
+  // When new items are dynamically added to the list, they don't have any event listeners attached yet.
+  // This is because the delete button doesn't exist when the page first loads, so we can't directly attach
+  // an event listener to it. To handle this, we use event delegation, which listens for click events
+  // on the parent element (the list), and when a click happens on a delete button (which is inside the list),
+  // it will trigger the handler for the delete action.
+  $("#transaction-list").on("click", ".delete", function () {
+    let amount = Number($(this).closest("li").find(".sum").text());
+
     if (amount < 0) {
       expense -= amount * -1;
     } else {
       income -= amount;
     }
+
     balance -= amount;
     $(this).closest("li").remove();
+
+    //Update the income and expense columns in chart; also the balance displayed
+    myChart.data.datasets[0].data[0] = income;
+    myChart.data.datasets[0].data[1] = expense;
+    $("#balance").html(balance);
     myChart.update();
   });
+
   const ctx = document.getElementById("myChart");
 
   const myChart = new Chart(ctx, {
@@ -51,7 +68,7 @@ $(document).ready(function () {
       datasets: [
         {
           label: "Euro",
-          data: [0, 0],
+          data: [0, 0], //Placeholders for income and expense array
           borderWidth: 1,
         },
       ],
