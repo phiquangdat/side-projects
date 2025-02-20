e95298c44f35b2752e306f7045bd673569e2c240
phiquangdat <datphi020603@gmail.com>
2025-02-20 21:57:20 +0200

    update the delete button

--- budget-tracker-app/script.js
+++ budget-tracker-app/script.js
@@ -1,55 +1,67 @@
-$(document).ready(function(){
-    let balance = 0;
-    let income = 0;
-    let expense = 0;
-    let type;
-    $("#addTransaction").click(function(){
-        let description = $("#description").val();
-        let sum = Number($("#sum").val());
-        balance += (sum);
-        if(sum < 0){
-            expense += (sum * -1);
-        }
-        else{
-            income += (sum);
-        }
-        $("#balance").html(balance);
-        $("#transaction-list").append(`
+$(document).ready(function () {
+  let balance = 0;
+  let income = 0;
+  let expense = 0;
+  let type;
+  $("#addTransaction").click(function () {
+    let description = $("#description").val();
+    let sum = Number($("#sum").val());
+    balance += sum;
+    if (sum < 0) {
+      expense += sum * -1;
+    } else {
+      income += sum;
+    }
+    $("#balance").html(balance);
+    $("#transaction-list").append(`
             <li class="${type}">
-                <p>${description}</p>
-                <p>${sum}</p>
+                <p class="description">${description}</p>
+                <p class="sum">${sum}</p>
                 <button class="delete">Poista</button>
             </li>
         `);
-        myChart.data.datasets[0].data[0] = income;
-        myChart.data.datasets[0].data[1] = expense;
-        myChart.update();
-        $("#description").val("");
-        $("#sum").val("");
 
-        $(".delete").click(function() {
-            $(this).closest("li").remove();
-        });
-    });
-    const ctx = document.getElementById('myChart');
+    //Add to the chart the data of income and expense columns
+    myChart.data.datasets[0].data[0] = income;
+    myChart.data.datasets[0].data[1] = expense;
+    myChart.update();
 
-    const myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['Income', 'Expenditure'],
-        datasets: [{
-          label: 'Euro',
+    $("#description").val("");
+    $("#sum").val("");
+    window.location.reload();
+  });
+
+  $(".delete").click(function () {
+    let amount = Number($(this).closest(".sum").val());
+    if (amount < 0) {
+      expense -= amount * -1;
+    } else {
+      income -= amount;
+    }
+    balance -= amount;
+    $(this).closest("li").remove();
+    myChart.update();
+  });
+  const ctx = document.getElementById("myChart");
+
+  const myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Income", "Expenditure"],
+      datasets: [
+        {
+          label: "Euro",
           data: [0, 0],
-          borderWidth: 1
-        }]
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
       },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    });
+    },
+  });
 });
-
