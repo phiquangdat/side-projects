450e74a5ba7697363f3cf13377a3ff5e5c7f1c56
phiquangdat <datphi020603@gmail.com>
2025-02-25 19:21:51 +0200

    create essential components of kanban board

---
+++ react/kanban-board/src/components/Card.jsx
@@ -0,0 +1,7 @@
+export default function Card({ item }) {
+  return (
+    <>
+      <li>{item}</li>
+    </>
+  );
+}
---
+++ react/kanban-board/src/components/List.jsx
@@ -0,0 +1,10 @@
+import Card from "./Card";
+export default function List() {
+  return (
+    <>
+      <ol>
+        <Card item={"Hello"} />
+      </ol>
+    </>
+  );
+}
